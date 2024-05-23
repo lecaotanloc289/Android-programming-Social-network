71867bc8fa2fcadc5599e9eba192bd131fc5121a
Lecaotanloc <lecaotanloc289@gmail.com>
2024-05-23 09:39:24 +0700

    add java android app

---
+++ app/src/main/java/com/home/intagramapp/StartActivity.java
@@ -0,0 +1,56 @@
+package com.home.intagramapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class StartActivity extends AppCompatActivity {
+
+    Button login,ragister;
+    FirebaseUser firebaseUser;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        if (firebaseUser != null){
+
+            startActivity(new Intent(StartActivity.this,MainActivity.class));
+            finish();
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+        login = findViewById(R.id.login_btn);
+        ragister = findViewById(R.id.Ragister);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(StartActivity.this,LoginActivity.class));
+
+
+            }
+        });
+
+        ragister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(StartActivity.this,RagisterActivity.class));
+
+            }
+        });
+
+    }
+}
